48b1f6c69c35db9686dee0e5ef2eecc702c3878e
Kokomyoheinhtet Unknown <kokomyoheinhtet@ascendcorp.com>
2020-08-24 12:09:33 +0700

    071 : App Wrapup

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,24 +1,44 @@
-import { User } from './User';
-import { Company } from './Company';
+import {User} from './User';
+import {Company} from './Company';
+
+export interface Mappable {
+    location: {
+        lat: number;
+        lng: number;
+    };
+
+    markerContent(): string;
+}
 
 export class CustomMap {
-  googleMap: google.maps.Map;
+    googleMap: google.maps.Map;
 
-  constructor() {
-    this.googleMap = new google.maps.Map(document.getElementById('map'), {
-      zoom: 2,
-      center: {
-        lat: 0,
-        lng: 0,
-      },
-    });
-  }
+    constructor() {
+        this.googleMap = new google.maps.Map(document.getElementById('map'), {
+            zoom: 2,
+            center: {
+                lat: 0,
+                lng: 0,
+            },
+        });
+    }
 
-  addUserMarker(user: User): void {
+    addMarker(mappable: Mappable): void {
+        const marker = new google.maps.Marker({
+            map: this.googleMap,
+            position: {
+                lat: mappable.location.lat,
+                lng: mappable.location.lng
+            }
+        });
 
-  }
+        marker.addListener('click', () => {
+            const infoWindow = new google.maps.InfoWindow({
+                content: mappable.markerContent()
+            });
+            
+            infoWindow.open(this.googleMap,marker);
+        });
 
-  addCompanyMarker(company: Company): void {
-
-  }
+    }
 }
--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,3 +1,10 @@
-import { CustomMap } from './CustomMap';
+import {CustomMap} from './CustomMap';
+import {User} from "./User";
+import {Company} from "./Company";
 
-new CustomMap();
+const user = new User();
+const company = new Company();
+const customMap = new CustomMap();
+
+customMap.addMarker(user);
+customMap.addMarker(company);
