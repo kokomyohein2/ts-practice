ff8feff69e5c29fafb82c49a88d55e1fecac99a2
Kokomyoheinhtet Unknown <kokomyoheinhtet@ascendcorp.com>
2020-08-28 10:05:41 +0700

    083 : Separating Swapping and Comparison

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,22 +1,7 @@
-class Sorter {
-    constructor(public collection: number[]) {
-    }
+import {NumbersCollection} from "./NumbersCollection";
+import {Sorter} from "./Sorter";
 
-    sort(): void {
-        const {length} = this.collection;
-
-        for (let i = 0; i < length; i++) {
-            for (let j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    const leftHand = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = leftHand;
-                }
-            }
-        }
-    }
-}
-
-const sorter = new Sorter([3, 2, 5, 1, 4]);
+const numbersCollection = new NumbersCollection([4, 2, 6, 3, 5, 1]);
+const sorter = new Sorter(numbersCollection);
 sorter.sort();
-console.log(sorter.collection);
+console.log(numbersCollection);
