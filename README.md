5ee8fc8601b6a9d475c060a57521c5f0e0fbd896
Kokomyoheinhtet Unknown <kokomyoheinhtet@ascendcorp.com>
2020-12-08 17:33:35 +0700

    86 : Sorting Arbitrary Collections

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 npm install -g typescript ts-node
+###To compile single file
+tsc index.ts
+
+###Install bundle 
+sudo npm install -g parcel-bundler
+parcel index.html
+
+###To compile the whole dir
+tsc --init
+and config tsconfig.json
+tsc
+
+###To compile and nodemon
+npm install nodemon concurrently
+see sort/package.json > script
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,13 @@
 import {NumbersCollection} from "./NumbersCollection";
 import {Sorter} from "./Sorter";
+import {CharactersCollections} from "./CharactersCollections";
 
-const numbersCollection = new NumbersCollection([4, 2, 6, 3, 5, 1]);
-const sorter = new Sorter(numbersCollection);
+// const numbersCollection = new NumbersCollection([4, 2, 6, 3, 5, 1]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection);
+
+const charactersCollection = new CharactersCollections('aXyybC');
+const sorter = new Sorter(charactersCollection);
 sorter.sort();
-console.log(numbersCollection);
+console.log(charactersCollection);
